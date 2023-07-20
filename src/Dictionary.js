fb87443cc4e1adfecc7036171f47fa7852a95714
SarahVanT <sarahvantilburg1@outlook.com>
2023-07-20 11:22:21 -0400

    Added axios and Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import './Dictionary.css';
 
 export default function Dictionary() {
     let [keyword, setkeyword] = useState("");
 
+    function handleResponse(response){
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        console.log(apiURL);
     }
 
     function handleKeywordChange(event){
