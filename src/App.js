17c86ed2f0357c52fab7bea8e0967587c094e7d5
SarahVanT <sarahvantilburg1@outlook.com>
2023-07-20 09:39:48 -0400

    Initial comment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './undraw_sharing_knowledge_03vp.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="img-fluid App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
